3291c196a2a10a5f16f3603e17cb8ca18da93114
Leke Lyu <46392207+leke-lyu@users.noreply.github.com>
2021-03-01 19:29:54 -0500

    Update plot2.R

--- rootToTipDistance/scripts/plot2.R
+++ rootToTipDistance/scripts/plot2.R
@@ -13,5 +13,6 @@ p1 <- ggplot(DF, aes(x=gene_f, y=coef, fill=catalogue_f)) +
   geom_bar(stat="identity", color="black", position=position_dodge()) + 
   geom_errorbar(aes(ymin=coef, ymax=coef+2*sd), width=.4, position=position_dodge(.9)) 
 p1 <- p1 + xlab("") + ylab("Mutation Rate") + labs(fill="")
+p1 <- p1 + scale_fill_manual(values=c("dark grey", "yellow", "blue", "green", "cyan", "purple"))
 ggsave(filename="mutationRate.pdf", width = 12, height = 6)
 
